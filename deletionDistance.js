d68dbb9ca718b5490370abbccbeef6edac4e0f1f
unknown <exgin3538@yahoo.com>
2020-09-02 20:43:59 -0400

    skeleton of distance

---
+++ deletionDistance.js
@@ -0,0 +1,8 @@
+/*
+the minimum number of characters needed to deleted in the first string
+to get the same string as str2
+*/
+function deletionDistance(str1, str2) {
+  let removed = 0;
+  if (str1.length === 0 && str2.length === 0) return 0;
+}
